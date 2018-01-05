8572403a2f0600556f15cf27c002694ba3ac7a24
AJ Wu <aj111743@gmail.com>
2018-01-05 14:30:42 +0800

    Remove git-ignore config

---
+++ config/connect.js
@@ -0,0 +1,16 @@
+export const FIREBASE_CONFIG = {
+  "apiKey": "AIzaSyBwI9fiqVTNyDAXZwF8It5RtCWrEE8kSdU",
+  "authDomain": "food-fe97a.firebaseapp.com",
+  "databaseURL": "https://food-fe97a.firebaseio.com",
+  "projectId": "food-fe97a",
+  "storageBucket": "food-fe97a.appspot.com",
+  "messagingSenderId": "928764386723"
+};
+export const FIREBASE_CONFIG_JEST = {
+	"apiKey": "AIzaSyDjQkGW-FnXiluowFogWg66jibNdstdiN8",
+	"authDomain": "food-jest.firebaseapp.com",
+	"databaseURL": "https://food-jest.firebaseio.com",
+	"projectId": "food-jest",
+	"storageBucket": "",
+	"messagingSenderId": "636992619487"
+};
